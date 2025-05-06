5821a4ac7c0495a5ff315a3583eb1f9f05458cf1
Maria Baron <166757495+MariaTBaronV@users.noreply.github.com>
2025-05-06 01:37:45 -0500

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from playwright.async_api import async_playwright
-import httpx
-import asyncio
-import os
-import uvicorn
 
-from procesar_cotizacion import procesar_cotizacion
-
-# 👉 Tu API KEY de 2Captcha
-API_KEY_2CAPTCHA = "1ce42240deeab8e84bb50b73fb2c77c9"
+from agentemotor_cotizador import cotizar_seguro
 
 app = FastAPI(title="Cotizar Seguro ASEDE")
-
-# Ruta raíz para pruebas
-@app.get("/")
-def read_root():
-    return {"mensaje": "API de Cotización ASEDE funcionando"}
 
 class CotizacionRequest(BaseModel):
     placa: str
@@ -34,14 +21,11 @@ class CotizacionRequest(BaseModel):
     correo: str
 
 @app.post("/cotizar")
-async def cotizar_seguro(datos: CotizacionRequest):
+def cotizar(datos: CotizacionRequest):
     try:
-        resultado = await procesar_cotizacion(datos)
+        resultado = cotizar_seguro(datos.dict())
         return resultado
+
     except Exception as e:
         print("ERROR EN EL SERVIDOR:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
